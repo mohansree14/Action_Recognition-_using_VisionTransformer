c3e791c212a48928c6b42166307961db15775e79
“mohansree14” <mohansree1709@gmail.com>
2025-08-11 04:26:47 +0100

    update

--- models/vit_model.py
+++ models/vit_model.py
@@ -2,4 +2,4 @@
 # Import model functions from separate files for each model
 from .timesformer_model import get_timesformer_model
 from .vit_base_model import get_vit_model
-from .slowfast_model import get_slowfast_model
+from .videomae_model import get_videomae_model
